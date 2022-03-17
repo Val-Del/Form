493672d53935d5307f0d78771e502c73fadebfd2
Val-Del <valentinbruneel1@gmail.com>
2022-03-17 20:20:05 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">   
+    <?php include("includes/head.php");
+    ?>
+<body>
+<main>
+  
+   <?php include("includes/header.php");
+    ?>
+  
+
+  <?php include("includes/form.php");
+
+   ?> 
+  
+   <?php include("includes/footer.php");
+   ?>
+   <!-- <style>
+   <?php include("includes/footer.css"); ?>
+   </style>  -->
+</main>
+</body>
+</html>
